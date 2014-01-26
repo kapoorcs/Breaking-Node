7dc6d0d53215da15e3670ee186e30ae7511fac0c
Chetan Kapoor <chetan.kapoor@venturepact.com>
2014-01-26 15:05:17 +0530

    Creating a Web Server

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+//
+
+http.createServer(function (request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<html><body>Home, URL was: ' + request.url + '</body></html>');
+    //Log whichever url is 
+    console.log(request.url);
+    
+}).listen(3000, 'localhost');
+
+console.log('Server running at http://localhost:3000/');
